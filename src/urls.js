826cccb1a0b3c88cfd803b561098e162500ea2a5
aditya-9613 <sarvaaditya13@gmail.com>
2024-05-20 12:11:26 +0530

    url correct

--- src/urls.js
+++ src/urls.js
@@ -1,2 +1,2 @@
-//export const baseURL = 'https://workers-blog.vercel.app';
-export const baseURL = 'http://localhost:5000';
+export const baseURL = 'https://wbbackend.onrender.com';
+//export const baseURL = 'http://localhost:5000';
